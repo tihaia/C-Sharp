42e3401ffc933340054ef92a89e684eb3b4b7562
tihaia <tanyatihaia03@gmail.com>
2025-10-02 00:18:41 +0300

    Lab 5 initial code

---
+++ Lab5/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+int client1_drink = 200;
+int client1_first = 250;
+int client1_total = client1_drink * 10 + client1_first * 20;
+Console.WriteLine("Итоговая стоимость " + client1_total);
+
+int client2_drink = 200;
+int client2_second = 300;
+int client2_total = client2_drink * 10 + client2_second * 30;
+Console.WriteLine("Итоговая стоимость " + client2_total);
