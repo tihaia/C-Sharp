91c189847a5ef614533c27aa8b113e32faa11a37
tihaia <145658291+tihaia@users.noreply.github.com>
2025-12-06 17:26:27 +0200

    Add files via upload

---
+++ Lab8a/Program.cs
@@ -0,0 +1,29 @@
+﻿
+// int FirstNum = InputInt("FirstNum");
+// int SecondNum = InputInt("SecondNum");
+// int ThirdNum = InputInt("ThirdNum");
+
+// int result = FirstNum * SecondNum * ThirdNum;
+// Console.WriteLine(result);
+
+// static int InputInt(string name)
+// {
+//     while (true)
+//     {
+//         Console.Write($"Enter {name}: ");
+//         string str = Console.ReadLine()!;
+//         bool parsed = int.TryParse(str, out int result);
+//         if (!parsed)
+//         {
+//             Console.WriteLine("Please enter a valid number");
+//             continue;
+//         }
+//         if (result <= 2)
+//         {
+//             Console.WriteLine("Number must be more than 2");
+//             continue;
+//         }
+
+//         return result;
+//     }
+// }
